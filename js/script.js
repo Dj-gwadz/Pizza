707b9ff4accf4bffdf01d2e3167804742088dcea
Moringa School <gwadajunior27@gmail.com>
2020-05-19 19:02:54 +0300

    add image to the hearder

--- js/script.js
+++ js/script.js
@@ -4,9 +4,40 @@
       this.pizzaSize = pizzaSize;
       this.pizzaCrust = pizzaCrust;
   }
-  Order.prototype.totallCost = function(){
-      if (this.pizzaSize === ""){}
+  Order.prototype.totalCost = function(){
+      if (this.pizzaSize === "small" && this.pizzaCrust === "crispy"){
+            cost = 1500
+      }
+      else if (this.pizzaSize === "medium" && this.pizzaCrust === "crispy"){
+            cost = 2000
+      }
+      else if (this.pizzaSize === "large" && this.pizzaCrust === "crispy"){
+            cost = 3500
+      }
+      else if (this.pizzaSize === "small" && this.pizzaCrust === "stufed"){
+           cost = 2000
+      }
+      else if (this.pizzaSize === "medium" && this.pizzaCrust === "stufed"){
+           cost = 3500
+      }
+      else if (this.pizzaSize === "large" && this.pizzaCrust === "stufed"){
+            cost = 4000
+      }
+      else if (this.pizzaSize === "small" && this.pizzaCrust === "gluten"){
+            cost = 2500
+      }
+      else if (this.pizzaSize === "medium" && this.pizzaCrust === "gluten"){
+            cost = 3500
+      }
+      else if (this.pizzaSize === "large" && this.pizzaCrust === "gluten"){
+            cost = 5000
+      }
+      else {
+        return "No selection"
+      }
+      return cost
   }
+
 
 
 
@@ -15,14 +46,31 @@ $(document).ready(function(){
     $("form#checkout").submit(function(event){
         event.preventDefault();
         var pizzaSize = $("#size").val();
-        alert(pizzaSize)
+      
         var pizzaCrust = $("#crust").val();
-        alert(pizzaCrust)
-        var topping = $("#topping").val();
-        alert(topping)
         
+        var topping = $("#topping").val();
+      
+        var quantity = $("#quantity").val();
+      
+        var delivery = $("#delivery").val();
+      
+
+        var newOrder = new Order(pizzaSize, pizzaCrust);
+        var totalCost = newOrder.totalCost() * quantity;
+        var totalDeliveryCost = newOrder.totalCost() * quantity + 500;
+       
+        
+        if (delivery === "self"){
+          alert("You have ordered " + pizzaSize + ", " + pizzaCrust + " pizza with " + topping + " topping. Your total cost will be " + totalCost)
+        }
+        else {
+          
+          alert("You have ordered " + pizzaSize + ", " + pizzaCrust + " pizza with " + topping + " topping. Your total cost will be " + totalDeliveryCost)
+
+        }
+
     });
-   
 
 });
 
