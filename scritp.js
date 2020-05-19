8b4d64dbd3a8f61a5dc1bdf5c741fdb37826d9c8
Moringa School <gwadajunior27@gmail.com>
2020-05-19 09:46:28 +0300

    having functions on my business logic

--- scritp.js
+++ scritp.js
@@ -1,6 +1,25 @@
 //Business logic
+function size(small, medium, large) {
+    this.small = small;
+    this.medium = medium;
+    this.large = large
+  }
+  function Crust(crispy, stuffed, gluten) {
+    this.crispy = crispy;
+    this.stuffed = stuffed;
+    this.gluten =  gluten;
+  }
+  function popping(onions, pepperoni, bacon, sausage) {
+    this.onions = onions;
+    this.pepperoni = pepperoni;
+    this.bacon = bacon;
+    this.sausage = sausage;
+  }
 
 
 
 
 //User interface logic
+$(document).ready(function(){
+
+});
